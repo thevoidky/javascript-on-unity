3f7d1f23fb5708388f6d611d4e5cdaa639064528
thevoidky <thevoidky@gmail.com>
2021-07-29 00:29:47 +0900

    Build Settings에 대한 인터페이스 추가

--- Editor/JavascriptBuilder.cs
+++ Editor/JavascriptBuilder.cs
@@ -9,6 +9,7 @@ using Newtonsoft.Json;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace Modules.Editor
 {
@@ -74,6 +75,144 @@ namespace Modules.Editor
 
         private Rect _rcControlArea;
         private Rect _rcButtonArea;
+
+        public static DefaultAsset[] RawScriptRoots
+        {
+            get => _buildSettings.rawScriptRoots;
+            set
+            {
+                var so = new SerializedObject(_buildSettings);
+
+                var rawScriptRoots = new ReorderableList(so, so.FindProperty("rawScriptRoots"));
+                rawScriptRoots.serializedProperty.ClearArray();
+
+                if (value == null)
+                {
+                    so.ApplyModifiedProperties();
+                    AssetDatabase.SaveAssets();
+                    return;
+                }
+
+                foreach (var asset in value)
+                {
+                    var path = AssetDatabase.GetAssetPath(asset);
+                    var fullPath = Path.Combine(ProjectPath, path);
+                    if (!Directory.Exists(fullPath))
+                    {
+                        Debug.LogError($"Input directory is not exist. ({asset.name})");
+                        return;
+                    }
+                }
+
+                for (var i = 0; i < value.Length; ++i)
+                {
+                    rawScriptRoots.serializedProperty.InsertArrayElementAtIndex(i);
+                    var element = rawScriptRoots.serializedProperty.GetArrayElementAtIndex(i);
+                    element.objectReferenceValue = value[i];
+                }
+
+                so.ApplyModifiedProperties();
+                AssetDatabase.SaveAssets();
+            }
+        }
+
+        public static DefaultAsset BuiltScriptRoot
+        {
+            get => _buildSettings.builtScriptRoot;
+            set
+            {
+                var so = new SerializedObject(_buildSettings);
+                var builtScriptRoot = so.FindProperty("builtScriptRoot");
+
+                if (value == null)
+                {
+                    builtScriptRoot.objectReferenceValue = value;
+                    so.ApplyModifiedProperties();
+                    AssetDatabase.SaveAssets();
+                    return;
+                }
+
+                var path = AssetDatabase.GetAssetPath(value);
+                var fullPath = Path.Combine(ProjectPath, path);
+                if (!Directory.Exists(fullPath))
+                {
+                    Debug.LogError($"Input directory is not exist. ({value.name})");
+                    return;
+                }
+
+                builtScriptRoot.objectReferenceValue = value;
+                so.ApplyModifiedProperties();
+                AssetDatabase.SaveAssets();
+            }
+        }
+
+        public static bool IsDevBuild
+        {
+            get => _buildSettings.isDevBuild;
+            set
+            {
+                var so = new SerializedObject(_buildSettings);
+                var isDevBuild = so.FindProperty("isDevBuild");
+
+                isDevBuild.boolValue = value;
+                so.ApplyModifiedProperties();
+                AssetDatabase.SaveAssets();
+            }
+        }
+
+        public static void Build()
+        {
+            var workspace =
+#if OOTL_DEV_LOCAL
+                Workspace;
+#else
+                RootFullPath;
+#endif
+            var match = Regex.Match(workspace, @"([a-zA-Z]):?[\\/](.*)");
+            var drive = $"/{(match.Success ? match.Groups[1].Value : string.Empty)}";
+            workspace = match.Success ? match.Groups[2].Value : workspace;
+
+            static string AssetPathToAbsolutePath(string assetPath)
+            {
+                return PathReplacer.Replace(
+                    Path.Combine(Application.dataPath, Regex.Replace(assetPath, @"^Assets[/\\]", "")),
+                    $"{Path.DirectorySeparatorChar}");
+            }
+
+            void GenerateMetadata()
+            {
+                var rawScriptPaths = _buildSettings.rawScriptRoots
+                    .SelectMany(defaultAsset =>
+                        Directory.GetFiles(AssetPathToAbsolutePath(AssetDatabase.GetAssetPath(defaultAsset)), @"*.js",
+                            SearchOption.AllDirectories));
+
+                var builtScriptRoot = AssetDatabase.GetAssetPath(_buildSettings.builtScriptRoot);
+
+                var output = new Dictionary<string, string>();
+                output.Add("path", AssetPathToAbsolutePath(builtScriptRoot));
+
+                var outputContent = JsonConvert.SerializeObject(output);
+                File.WriteAllText(OutputPath, outputContent);
+
+                var entry = new Dictionary<string, string>();
+                foreach (var scriptPath in rawScriptPaths)
+                {
+                    var filename = Path.GetFileName(scriptPath);
+                    entry.Add(Path.GetFileNameWithoutExtension(filename), scriptPath);
+                }
+
+                var entryContent = JsonConvert.SerializeObject(entry);
+                File.WriteAllText(EntryPath, entryContent, Encoding.UTF8);
+            }
+
+            GenerateMetadata();
+
+            workspace = PathReplacer.Replace(workspace, $"{Path.DirectorySeparatorChar}").Replace(":", "");
+            var isDevBuild = _buildSettings.isDevBuild.ToString().ToLower();
+            var parameters = $"'{workspace}' {drive} {isDevBuild}";
+
+            Process.Start(BuilderPath, parameters);
+        }
 
         [MenuItem("Javascript on Unity/Open Javascript Builder")]
         private static void ShowWindow()
@@ -190,60 +329,6 @@ namespace Modules.Editor
 
             AssetDatabase.Refresh();
 #endif
-        }
-
-        private static void Build()
-        {
-            var workspace =
-#if OOTL_DEV_LOCAL
-                Workspace;
-#else
-                RootFullPath;
-#endif
-            var match = Regex.Match(workspace, @"([a-zA-Z]):?[\\/](.*)");
-            var drive = $"/{(match.Success ? match.Groups[1].Value : string.Empty)}";
-            workspace = match.Success ? match.Groups[2].Value : workspace;
-
-            static string AssetPathToAbsolutePath(string assetPath)
-            {
-                return PathReplacer.Replace(
-                    Path.Combine(Application.dataPath, Regex.Replace(assetPath, @"^Assets[/\\]", "")),
-                    $"{Path.DirectorySeparatorChar}");
-            }
-
-            void GenerateMetadata()
-            {
-                var rawScriptPaths = _buildSettings.rawScriptRoots
-                    .SelectMany(defaultAsset =>
-                        Directory.GetFiles(AssetPathToAbsolutePath(AssetDatabase.GetAssetPath(defaultAsset)), @"*.js",
-                            SearchOption.AllDirectories));
-
-                var builtScriptRoot = AssetDatabase.GetAssetPath(_buildSettings.builtScriptRoot);
-
-                var output = new Dictionary<string, string>();
-                output.Add("path", AssetPathToAbsolutePath(builtScriptRoot));
-
-                var outputContent = JsonConvert.SerializeObject(output);
-                File.WriteAllText(OutputPath, outputContent);
-
-                var entry = new Dictionary<string, string>();
-                foreach (var scriptPath in rawScriptPaths)
-                {
-                    var filename = Path.GetFileName(scriptPath);
-                    entry.Add(Path.GetFileNameWithoutExtension(filename), scriptPath);
-                }
-
-                var entryContent = JsonConvert.SerializeObject(entry);
-                File.WriteAllText(EntryPath, entryContent, Encoding.UTF8);
-            }
-
-            GenerateMetadata();
-
-            workspace = PathReplacer.Replace(workspace, $"{Path.DirectorySeparatorChar}").Replace(":", "");
-            var isDevBuild = _buildSettings.isDevBuild.ToString().ToLower();
-            var parameters = $"'{workspace}' {drive} {isDevBuild}";
-
-            Process.Start(BuilderPath, parameters);
         }
     }
 }
